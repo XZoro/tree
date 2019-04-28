c81b63c62dc94ca4eb32c67a45f431639e800a2a
xiezhongqing <xiezhongqing@jinyafu.com>
2019-04-28 16:20:56 +0800

    HAHA

---
+++ src/main/java/com/springboot/demo/mapper/CategoryMapper.java
@@ -0,0 +1,27 @@
+package com.springboot.demo.mapper;
+
+import com.springboot.demo.pojo.Category;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface CategoryMapper {
+
+    @Select("select * from category_")
+    List<Category> findAll();
+
+    @Insert("insert into category_ (name) values (#{name})")
+    public int save(Category category);
+
+    @Delete("delete from category_ where id=#{id}")
+    public void delete(int id);
+
+    @Update("update category_ set name=#{name} where id=#{id}")
+    public int update(Category category);
+
+    @Select("select * from category_ where id = #{id}")
+    public Category get(int id);
+}
+
+
